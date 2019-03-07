45d4a3cd8eb136b6070b9f9a8e37f67a6a66e5dd
Hunter Kern <Hunterkern2015@u.northwestern.edu>
2019-03-07 12:08:34 -0600

    Update recipe_transform.py
    Added dictionary of  food categories

--- recipe_transform.py
+++ recipe_transform.py
@@ -16,6 +16,22 @@ stopwords.extend(custom_stopwords)
 ingredient_switches = {}
 
 
+ingredient_categories = {
+    "healthy_fats": ['olive oil', 'sunflower oil', 'soybean oil', 'corn oil',  'sesame oil',  'peanut oil'],
+    "unhealthy_fats": ['butter', 'lard', 'shortening', 'canola oil', 'margarine',  'coconut oil',  'tallow',  'cream', 'milk fat',  'palm oil',  'palm kemel oil',  'chicken fat',  'hydrogenated oils'],
+    "healthy_protein": [ 'peas',  'beans', 'eggs', 'crab', 'fish','chicken', 'tofu', 'liver', 'turkey'],
+    "unhealthy_protein": ['ground beef', 'beef', 'pork', 'lamb'],
+    "healthy_dairy": ['fat free milk', 'low fat milk', 'yogurt',  'low fat cheese'],
+    "unhealthy_dairy": ['reduced-fat milk', 'cream cheese', 'whole milk', 'butter', 'cheese', 'whipped cream', 'sour cream'],
+    "healthy_salts": ['low sodium soy sauce', 'sea salt', 'kosher salt'],
+    "unhealthy_salts": ['soy sauce', 'table salt', 'salt'],
+    "healthy_grains": ['oat cereal', 'wild rice', 'oatmeal', 'whole rye', 'buckwheat', 'rolled oats', 'quinoa','bulgur', 'millet', 'brown rice', 'whole wheat pasta'],
+    "unhealthy_grains": ['macaroni', 'noodles', 'spaghetti', 'white rice', 'white bread', 'regular white pasta'],
+    "healthy_sugars": ['real fruit jam', 'fruit juice concentrates', 'monk fruit extract', 'cane sugar', 'molasses', 'brown rice syrup' 'stevia', 'honey', 'maple syrup', 'agave syrup', 'coconut sugar', 'date sugar', 'sugar alcohols', 'brown sugar'],
+    "unhealthy_sugars": ['aspartame', 'acesulfame K', 'sucralose', 'white sugar', 'corn syrup', 'chocolate syrup']
+}
+# categorized foods found at https://github.com/olivergoodman/food-recipes/blob/master/transforms.py
+
 # recipe class definition
 class Recipe:
     def __init__(self, url):
@@ -285,7 +301,7 @@ baking_healthy_substitutions_exceptions = {
 # vegetarian substitutions dictionaries
 
 vegetarian_substitutions_names = {
-    "chicken": {"substitutions": [functools.partial(change_name, "tofu")]},
+    "chicken": {"substitutions": [functools.partial(change_name, "eggplant")]},
     "pork": {"substitutions": [functools.partial(change_name, "tofu")]},
     "beef": {"substitutions": [functools.partial(change_name, "lentils")]},
     "steak": {"substitutions": [functools.partial(change_name, "mushroom"),
