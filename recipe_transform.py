3ce368b2816d5e6df8283121776221a87748b5d0
Hunter Kern <Hunterkern2015@u.northwestern.edu>
2019-03-11 13:03:09 -0500

    added spice and herb to categories

--- recipe_transform.py
+++ recipe_transform.py
@@ -40,18 +40,33 @@ UNITS = ['tablespoon', 'teaspoon', 'cup', 'clove', 'pound']
 
 INGREDIENT_CATEGORIES = {
     'healthy_fats': ['olive oil', 'sunflower oil', 'soybean oil', 'corn oil',  'sesame oil',  'peanut oil'],
-    'unhealthy_fats': ['butter', 'lard', 'shortening', 'canola oil', 'margarine',  'coconut oil',  'tallow',  'cream', 'milk fat',  'palm oil',  'palm kemel oil',  'chicken fat',  'hydrogenated oils'],
+    'unhealthy_fats': ['butter', 'lard', 'shortening', 'canola oil', 'margarine',  'coconut oil',  'tallow',  'cream',
+                       'milk fat',  'palm oil',  'palm kemel oil',  'chicken fat',  'hydrogenated oils'],
     'healthy_protein': ['peas',  'beans', 'eggs', 'crab', 'fish', 'chicken', 'tofu', 'liver', 'turkey'],
     'unhealthy_protein': ['ground beef', 'beef', 'pork', 'lamb'],
-    'meat': ['scallop', 'sausage', 'bacon', 'beef', 'pork', 'lamb', 'crab', 'fish', 'chicken', 'turkey', 'liver', 'duck', 'tuna', 'lobster', 'salmon', 'shrimp', 'crayfish', 'crawfish', 'ribs', 'pheasant', 'escargot', 'snail', 'bass', 'sturgeon', 'trout', 'flounder', 'carp', 'quail', 'goose'],
+    'meat': ['scallop', 'sausage', 'bacon', 'beef', 'pork', 'lamb', 'crab', 'fish', 'chicken', 'turkey', 'liver',
+             'duck', 'tuna', 'lobster', 'salmon', 'shrimp', 'crayfish', 'crawfish', 'ribs', 'pheasant', 'escargot',
+             'snail', 'bass', 'sturgeon', 'trout', 'flounder', 'carp', 'quail', 'goose'],
     'healthy_dairy': ['fat free milk', 'low fat milk', 'yogurt',  'low fat cheese'],
-    'unhealthy_dairy': ['reduced-fat milk', 'cream cheese', 'whole milk', 'butter', 'cheese', 'whipped cream', 'sour cream'],
+    'unhealthy_dairy': ['reduced-fat milk', 'cream cheese', 'whole milk', 'butter', 'cheese', 'whipped cream',
+                        'sour cream'],
     'healthy_salts': ['low sodium soy sauce', 'sea salt', 'kosher salt'],
     'unhealthy_salts': ['soy sauce', 'table salt', 'salt'],
-    'healthy_grains': ['oat cereal', 'wild rice', 'oatmeal', 'whole rye', 'buckwheat', 'rolled oats', 'quinoa', 'bulgur', 'millet', 'brown rice', 'whole wheat pasta'],
+    'healthy_grains': ['oat cereal', 'wild rice', 'oatmeal', 'whole rye', 'buckwheat', 'rolled oats', 'quinoa',
+                       'bulgur', 'millet', 'brown rice', 'whole wheat pasta'],
     'unhealthy_grains': ['macaroni', 'noodles', 'spaghetti', 'white rice', 'white bread', 'regular white pasta'],
-    'healthy_sugars': ['real fruit jam', 'fruit juice concentrates', 'monk fruit extract', 'cane sugar', 'molasses', 'brown rice syrup' 'stevia', 'honey', 'maple syrup', 'agave syrup', 'coconut sugar', 'date sugar', 'sugar alcohols', 'brown sugar'],
-    'unhealthy_sugars': ['aspartame', 'acesulfame K', 'sucralose', 'white sugar', 'corn syrup', 'chocolate syrup']
+    'healthy_sugars': ['real fruit jam', 'fruit juice concentrates', 'monk fruit extract', 'cane sugar', 'molasses',
+                       'brown rice syrup' 'stevia', 'honey', 'maple syrup', 'agave syrup', 'coconut sugar',
+                       'date sugar', 'sugar alcohols', 'brown sugar'],
+    'unhealthy_sugars': ['aspartame', 'acesulfame K', 'sucralose', 'white sugar', 'corn syrup', 'chocolate syrup'],
+    'spice': ['ajwain', 'allspice', 'almond meal', 'anise seed', 'annatto seed', 'arrowroot powder', 'cacao',
+              'bell pepper', 'beetroot powder', 'chia seeds', 'cloves', 'chiles', 'cinnamon', 'cloves', 'coriander',
+              'dill seed', 'garlic', 'ginger', 'mustard', 'onion', 'paprika', 'cayenne', 'pepper', 'red pepper',
+              'shallots', 'star anise', 'turmeric', 'vanilla extract'],
+    'herb': ['basil', 'bay leaves', 'celery flakes', 'chervil', 'cilantro', 'curry', 'dill weed', 'dried chives',
+             'epatoze', 'file powder', 'kaffire lime', 'lavender', 'lemongrass', 'mint', 'oregano', 'parsley',
+             'rosemary', 'sage', 'tarragon', 'thyme']
+
 }
 
 SYNONYMS = {
@@ -852,7 +867,7 @@ def add_ingredient(ingredient_text):
         if meat in full_name:
             category = meat
 
-    for key,val in INGREDIENT_CATEGORIES.items():
+    for key, val in INGREDIENT_CATEGORIES.items():
         if (full_name in val or name in val) and category is None:
             category = key
 
